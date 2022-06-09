b7c99b9fe35a60be730ff4b5b34dc456f1e6fe8e
Chitakov <chitakov2606nikita@gmail.com>
2022-06-09 18:10:13 +0300

    Add project files.

---
+++ Lab2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите корень n-ой степени: ");
+            double n = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите точность: ");
+            double eps = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Результат: " + SqrtN(n, number, eps));
+            Console.WriteLine("Результат, возвращаемый методом Math.Pow: " + Math.Pow(number, 1 / n));
+            Console.ReadKey();
+        }
+        static double SqrtN(double n, double number, double eps)
+        {
+            var x0 = number / n;
+            var x1 = (1 / n) * ((n - 1) * x0 + number / Math.Pow(x0, n - 1));
+
+            while (Math.Abs(x1 - x0) > eps)
+            {
+                x0 = x1;
+                x1 = (1 / n) * ((n - 1) * x0 + number / Math.Pow(x0, n - 1));
+            }
+            return x1;
+        }
+    }
+}
---
+++ Lab2_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if(number == 0)
+            {
+                Console.WriteLine("Число в двоичной системе счисления - 0");
+                Console.ReadLine();
+            }
+            else if (number > 0)
+            {
+                string result = "";
+                int temp = number;
+                while (number > 0)
+                {
+                    temp = number % 2;
+                    if (temp == 0) result += "0";
+                    else result += "1";
+                    number /= 2;
+                }
+                char[] charArray = result.ToCharArray();
+                Array.Reverse(charArray);
+                string result1 = new string(charArray);
+                Console.WriteLine("Число в двоичной системе счисления - " + result1);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Число не является неотрицательным");
+                Console.ReadLine();
+            }
+        }
+    }
+}
